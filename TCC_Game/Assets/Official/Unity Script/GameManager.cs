6b8d40cfecc679711212056c1e505d0d04ff223c
Rafael <rafael.colpani14@outlook.com>
2023-03-30 11:40:53 -0300

    Dev Mode + Summer Level

--- TCC_Game/Assets/Official/Unity Script/GameManager.cs
+++ TCC_Game/Assets/Official/Unity Script/GameManager.cs
@@ -6,12 +6,21 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Dev Mode")]
+    [SerializeField] bool devMode;
+    [SerializeField] TMP_Text devText;
+
+    [Space(5)]
     [Header("FPS Display")]
     [SerializeField] TMP_Text fpsText;
-
-
+    [SerializeField] TMP_Text fpsNameFrame;
     private int lastFPS;
     private float[] frameDeltaTime;
+
+    [Space(5)]
+    [Header("Scene")]
+    [SerializeField] TMP_Text sceneNameFrame;
+    [SerializeField] TMP_Text sceneText;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +34,36 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //FPS Log
-        frameDeltaTime[lastFPS] = Time.deltaTime;
-        lastFPS = (lastFPS + 1) % frameDeltaTime.Length;
-        fpsText.text = Mathf.RoundToInt(CalculateFPS()).ToString();
-    
+        //Dev mode log
+        if(devMode)
+        {
+            devText.enabled = true;
+
+            //FPS Log
+            fpsNameFrame.enabled = true;
+            fpsText.enabled = true;
+            
+            frameDeltaTime[lastFPS] = Time.deltaTime;
+            lastFPS = (lastFPS + 1) % frameDeltaTime.Length;
+            fpsText.text = Mathf.RoundToInt(CalculateFPS()).ToString();
+
+            //Scene Log
+            sceneNameFrame.enabled = true;
+            sceneText.enabled = true;
+            
+            Scene scene = SceneManager.GetActiveScene();
+            sceneText.text = scene.name;
+
+        }
+
+        if(!devMode)
+        {
+            sceneNameFrame.enabled = false;
+            fpsNameFrame.enabled = false;
+            devText.enabled = false;
+            sceneText.enabled = false;
+            fpsText.enabled = false;
+        }
     }
 
     float CalculateFPS()
