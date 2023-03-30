aaa485e8806c2bfdc504943107275787abb5afe7
Gabriel da Silva Barros <silvagabr57@gmail.com>
2023-03-30 11:28:29 -0300

    UI inventory scripts update

--- TCC_Game/Assets/Prototype/UI/Scripts/Inventory/InventoryManager.cs
+++ TCC_Game/Assets/Prototype/UI/Scripts/Inventory/InventoryManager.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
+    public GameObject InventoryItemPrefab;
     public void AddItem(Item item)
     {
+        // ACHAR UM SLOT VAZIO
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponent<InventoryItem>();
 
+            if(itemInSlot == null)
+            {
+                // Pode adicionar o item fi
+                SpawnNewItem(item, slot);
+                return; // stops code execution
+            }
+        }
     }
 
     void SpawnNewItem(Item item, InventorySlot slot)
     {
-
+        GameObject newItemGo = Instantiate(InventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        inventoryItem.InitializeItem(item);
+        
     }
 }
