5d706a35617671bfe4e57b79189aadeaf689d394
Rafael <rafael.colpani14@outlook.com>
2023-03-30 00:51:29 -0300

    rename to "FPS Display + Level Designer (Summer)"

--- TCC_Game/Assets/Official/Unity Script/GameManager.cs
+++ TCC_Game/Assets/Official/Unity Script/GameManager.cs
@@ -2,44 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("FPS Display")]
-    [SerializeField] TMP_Text fpsText;
-
-
-    private int lastFPS;
-    private float[] frameDeltaTime;
-
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("GameManager Start");
-
-        //FPS Log
-        frameDeltaTime = new float[50];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //FPS Log
-        frameDeltaTime[lastFPS] = Time.deltaTime;
-        lastFPS = (lastFPS + 1) % frameDeltaTime.Length;
-        fpsText.text = Mathf.RoundToInt(CalculateFPS()).ToString();
-    
-    }
-
-    float CalculateFPS()
-    {
-        float totalFPS = 0f;
-        foreach (float deltaTime in frameDeltaTime)
-        {
-            totalFPS += deltaTime;
-        }
-
-        return frameDeltaTime.Length / totalFPS;
+        
     }
 }
