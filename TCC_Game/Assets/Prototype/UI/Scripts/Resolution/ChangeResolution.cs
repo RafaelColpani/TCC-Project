9c05d98461303ab492a750457743487db04a3ae3
Gabriel da Silva Barros <tiolen@protonmail.com>
2023-04-14 18:08:17 -0300

    UI behaviour

--- TCC_Game/Assets/Prototype/UI/Scripts/Resolution/ChangeResolution.cs
+++ TCC_Game/Assets/Prototype/UI/Scripts/Resolution/ChangeResolution.cs
@@ -6,57 +6,60 @@ using TMPro;
 
 public class ChangeResolution : MonoBehaviour
 {
-    [SerializeField] TMP_Dropdown resolutionDropdown;
-
     Resolution[] resolutions;
-    List<Resolution> filteredResolutions;
 
-    float currentRefreshRate;
-    int currentResolutionIndex = 0;
-
+    [SerializeField] TMP_Dropdown resolutionDropdown;
     public bool fullscreen;
 
+    int filteredIndexDistance = 0;
     private void Start()
     {
         resolutions = Screen.resolutions;
-        filteredResolutions = new List<Resolution>();
 
         resolutionDropdown.ClearOptions();
-        currentRefreshRate = Screen.currentResolution.refreshRate;
 
-        for (int i = 0; i < resolutions.Length; i++)
+        List<string> _options = new();
+
+        int currentResolutionIndex = 0;
+
+        int i = 0;
+        for (int j = 0; j < resolutions.Length; j++)
         {
-            if (resolutions[i].refreshRate == currentRefreshRate && resolutions[i].width >= 1024 && resolutions[i].height >= 768)
+            if (resolutions[j].width >= 1024 && resolutions[j].height >= 768)
             {
-                filteredResolutions.Add(resolutions[i]);
+                string option = resolutions[j].width + " x " + resolutions[j].height;
+                _options.Add(option);
+                
+                if (resolutions[j].width == Screen.width &&
+                resolutions[j].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                    SetResolution(currentResolutionIndex);
+                }
+                filteredIndexDistance++; i++;
             }
         }
-
-        List<string> options = new List<string>();
-        for (int i = 0; i < filteredResolutions.Count; i++)
-        {
-            string resolutionOption = $"{filteredResolutions[i].width}x{filteredResolutions[i].height}"; // + $"{filteredResolutions[i].refreshRate} Hz";
-            options.Add(resolutionOption);
-            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.AddOptions(_options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
     }
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = filteredResolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, fullscreen);
+        if(Screen.width <= 1024 && Screen.height <= 768)
+            Screen.SetResolution(1024, 768, Screen.fullScreen);
+        else
+        {
+            Resolution resolution = resolutions[resolutionIndex + resolutions.Length - filteredIndexDistance];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+            Debug.Log($"index resolucao atual: {resolutionIndex}" +
+                            $"\n{resolution.width}, {resolution.height}");
+        }
     }
 
     public void ToggleFullscreen(bool f)
     {
         f = !f;
-        fullscreen = f;
-        Screen.SetResolution(Screen.width, Screen.height, fullscreen);
+        Screen.fullScreen = f;
     }
 }
---
+++ TCC_Game/Assets/Prototype/UI/Scripts/Resolution/ResolutionFix.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionFix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Screen.currentResolution.width < 1024)
+        {
+            Screen.SetResolution(1024, 768, Screen.fullScreen);
+        }
+    }
+}
