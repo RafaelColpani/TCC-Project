a396165002bc17e4c5ed177c0e619ae698d65f58
Henrique Assis <henriquebatista061@gmail.com>
2023-09-18 18:07:56 -0300

    feat: braços se movendo e ajustes

--- TCC_Game/Assets/General Assets/Characters/World Script/ProceduralArms.cs
+++ TCC_Game/Assets/General Assets/Characters/World Script/ProceduralArms.cs
@@ -8,17 +8,53 @@ using System.Linq;
 [System.Serializable]
 public class ArmsTargets
 {
+    #region Inspector Vars
     [Tooltip("The target that he arm effector will follow")]
-    public Transform target;
+    public Transform effectorTarget;
     [Tooltip("The effector that will link the bones")]
     public Transform effector;
+    [Tooltip("The position that the arm will aim while moving ahead")]
+    public Transform aheadTarget;
+    [Tooltip("The position that the arm will aim while moving behind the body")]
+    public Transform behindTarget;
     [Tooltip("The height that the target will be, based on body position")]
     public float targetHeightOffset;
     [Tooltip("Each arm will move in a different direction, problably accordingly to the oposite leg, so it's " +
         "recommended to mark this as oposite to the same side leg (Example: if right leg is true, the right arm must be false)")]
     public bool isEven;
+    #endregion
+
+    #region Private Vars
+    private bool isMovingAhead;
+    private bool isMovingBehind;
+    #endregion
+
+    #region Getters
+    public bool IsMovingAhead
+    {
+        get { return this.isMovingAhead; }
+    }
+
+    public bool IsMovingBehind
+    {
+        get { return this.isMovingBehind; }
+    }
+    #endregion
+
+    #region Setters
+    public void SetIsMovingAhead(bool value = true)
+    {
+        this.isMovingAhead = value;
+    }
+
+    public void SetIsMovingBehind(bool value = true)
+    {
+        this.isMovingBehind = value;
+    }
+    #endregion
 }
 
+[RequireComponent(typeof(ProceduralLegs))]
 public class ProceduralArms : MonoBehaviour
 {
     #region Inspector VARs
@@ -28,6 +64,9 @@ public class ProceduralArms : MonoBehaviour
     [HeaderPlus(" ", "- ARMS -", (int)HeaderPlusColor.yellow)]
     [Tooltip("The value of the arc movement that the arm will realize")]
     [SerializeField] private float arcValue;
+    [SerializeField] private float armArcHeight;
+    [SerializeField] private float armArcSpeed;
+    [SerializeField] private float armMoveSpeed;
     #endregion
 
     #region Private VARs
@@ -35,6 +74,8 @@ public class ProceduralArms : MonoBehaviour
     private ProceduralLegs proceduralLegs;
 
     private Transform body;
+
+    private float lerpArm = 0;
     #endregion
 
     #region Unity Methods
@@ -51,7 +92,7 @@ public class ProceduralArms : MonoBehaviour
         if (PauseController.GetIsPaused()) return;
 
         //CalculateTargetsHeight();
-
+        MoveArms();
     }
     #endregion
 
@@ -71,7 +112,67 @@ public class ProceduralArms : MonoBehaviour
         // is not moving any leg
         if (!proceduralLegs.GetIsWalking()) return;
 
+        var oddIsWalking = proceduralLegs.OddIsWalking;
 
+        foreach (var arm in armsTargets)
+        {
+            // the opposite arm move ahead
+            var moveAhead = (oddIsWalking && arm.isEven) || (!oddIsWalking && !arm.isEven);
+
+            if (!ArmIsMovingInRightDirection(arm, moveAhead))
+            {
+                if (moveAhead)
+                {
+                    arm.SetIsMovingAhead(true);
+                    arm.SetIsMovingBehind(false);
+                }
+                    
+                else
+                {
+                    arm.SetIsMovingAhead(false);
+                    arm.SetIsMovingBehind(true);
+                }
+
+                lerpArm = 0;
+            }
+
+            float height = armArcHeight;
+            float arc = Mathf.Sin(lerpArm * Mathf.PI) * height;
+            lerpArm += Time.fixedDeltaTime * armArcSpeed;
+
+
+            var newPosition = Vector3.Lerp( arm.effectorTarget.position,
+                                            moveAhead ? arm.aheadTarget.position : arm.behindTarget.position,
+                                            armMoveSpeed * Time.deltaTime);
+
+            print(lerpArm);
+            if (lerpArm < 1)
+            {
+                if (moveAhead)
+                    newPosition.y += arc;
+                else
+                    newPosition.y += arc;
+            }
+
+            else
+                newPosition = moveAhead ? arm.aheadTarget.position : arm.behindTarget.position;
+
+            arm.effectorTarget.position = newPosition;
+        }
+    }
+
+    private bool ArmIsMovingInRightDirection(ArmsTargets arm, bool moveAhead)
+    {
+        if (moveAhead)
+        {
+            return arm.IsMovingAhead == true;
+        }
+
+        else
+        {
+            return arm.IsMovingBehind == true;
+        }
+        
     }
     #endregion
 }
