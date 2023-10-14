c05c2651942cbf5f76452bda7f15a153f97192f8
Rafael <rafael.colpani14@outlook.com>
2023-10-14 18:51:12 -0300

    Scene - 1 Finalizada
    - Puzzle
    - Abertura de porta e de ponte
    - Evento com musica
    - Size de câmera
    - Falta colocar um transição entre cenas
    
    - Está totalmente testável esse fase

---
+++ TCC_Game/Assets/General Assets/Test Game Mechanics/AlterarOrthoSize.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class AlterarOrthoSize : MonoBehaviour
+{
+    public string tagDoJogador = "Player";
+    public CinemachineVirtualCamera virtualCamera;
+    public float novoOrthoSize = 5.0f; // O novo valor de Lens Ortho Size
+    public float velocidadeTransicao = 2.0f; // Velocidade da transição (ajuste conforme necessário)
+
+    private float orthoSizeOriginal; // O valor original de OrthographicSize
+    private bool emTransicao = false; // Para controlar o estado de transição
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(tagDoJogador) && !emTransicao)
+        {
+            // Verifica se o objeto que passou possui a tag do jogador
+            if (virtualCamera != null)
+            {
+                orthoSizeOriginal = virtualCamera.m_Lens.OrthographicSize;
+                emTransicao = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (emTransicao)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, novoOrthoSize, velocidadeTransicao * Time.deltaTime);
+
+            // Quando a transição estiver próxima do valor desejado, finalize
+            if (Mathf.Approximately(virtualCamera.m_Lens.OrthographicSize, novoOrthoSize))
+            {
+                emTransicao = false;
+            }
+        }
+    }
+}
