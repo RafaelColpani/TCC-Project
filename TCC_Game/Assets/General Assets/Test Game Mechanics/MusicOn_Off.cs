19cc26060822680a1978686bd2ebc96bd43a2f7d
Rafael <rafael.colpani14@outlook.com>
2023-10-17 16:01:34 -0300

    Feedback de texto para a scene 1 - ok

--- TCC_Game/Assets/General Assets/Test Game Mechanics/MusicOn_Off.cs
+++ TCC_Game/Assets/General Assets/Test Game Mechanics/MusicOn_Off.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Collider2D), typeof(AudioSource))]
 
@@ -10,11 +11,22 @@ public class MusicOn_Off : MonoBehaviour
     public bool ativarMusica = true;
     private bool musicaTocada = false;
     private AudioSource audioSource;
+    public TextMeshProUGUI textoTMP; // Adicione a referência ao TextMeshPro
+
+    public bool _dicaOnOff = false;
+    public GameObject objetoDica;
+    public TextMeshProUGUI textoDica;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.playOnAwake = false;
+
+        // Desative o texto TMP no início
+        if (textoTMP != null)
+        {
+            textoTMP.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,13 +40,48 @@ public class MusicOn_Off : MonoBehaviour
                     audioSource.clip = musica;
                     audioSource.Play();
                     musicaTocada = true;
+
+                    
                 }
+
+                // Ative o texto TMP
+                if (textoTMP != null)
+                {
+                    textoTMP.gameObject.SetActive(true);
+                }
+
+                
                 else
                 {
                     // Se a música já foi tocada, reinicie-a
                     audioSource.Stop();
                     audioSource.Play();
                 }
+            }
+
+            if(_dicaOnOff = true)
+            {   
+            
+                {
+                    objetoDica.SetActive(true);
+                    textoDica.gameObject.SetActive(true);
+                }
+
+            }
+        }
+
+        
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Desative o texto TMP quando o jogador sair do Collider Trigger
+            if (textoTMP != null)
+            {
+                textoTMP.gameObject.SetActive(false);
             }
         }
     }
