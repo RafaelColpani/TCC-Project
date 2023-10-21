fabc1d0606c4fb54e40706ffb2d05d32054d7bdc
Rafael <rafael.colpani14@outlook.com>
2023-10-21 02:26:40 -0300

    Ajustes nos tutoriais, Tela de Game Over, Base, Puzzle 1

---
+++ TCC_Game/Assets/General Assets/Test Game Mechanics/StarWarsIntro.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
+
+public class StarWarsIntro : MonoBehaviour
+{
+    [Header("Text Settings")]
+    [SerializeField] TextMeshProUGUI introText; // Referência ao componente TextMeshPro onde o texto será exibido
+    [SerializeField] float scrollSpeed = 10.0f; // Velocidade de rolagem do texto
+
+    [Header("Text")]
+    [SerializeField] string text;
+
+    [Header("Scene")]
+    [SerializeField] string sceneName;
+
+    private bool isScrolling = false;
+
+    private void Start()
+    {
+        // Inicia a rolagem do texto
+        StartCoroutine(ScrollText());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Pule a introdução quando a tecla Espaço for pressionada
+            SkipIntro();
+        }
+    }
+
+    private IEnumerator ScrollText()
+    {
+        isScrolling = true;
+        introText.text = ""; // Limpa o texto no início
+
+        string intro = text;
+
+        for (int i = 0; i < intro.Length; i++)
+        {
+            introText.text += intro[i];
+            yield return new WaitForSeconds(1 / scrollSpeed);
+        }
+
+        // Aguarde um momento antes de desativar a rolagem
+        yield return new WaitForSeconds(2.0f);
+        isScrolling = false;
+
+        // Opcional: Inicie alguma ação, como carregar o próximo nível
+    }
+
+    private void SkipIntro()
+    {
+        if (isScrolling)
+        {
+            // Se estiver rolando, pule a introdução e complete o texto
+            StopAllCoroutines();
+            introText.text = text;
+            isScrolling = false;
+
+            // Opcional: Inicie alguma ação, como carregar o próximo nível
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
