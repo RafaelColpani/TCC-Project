ef4be1cff145b9af8d55ecdb8068f6860119ebaa
RafaelColpani <rafael.colpani14@outlook.com>
2023-10-24 01:57:08 -0300

    Fix: Ajuste provisório para a ultima cena do jogo

--- TCC_Game/Assets/General Assets/Artifacts/ArtifactInteractionProto.cs
+++ TCC_Game/Assets/General Assets/Artifacts/ArtifactInteractionProto.cs
@@ -11,17 +11,26 @@ public class ArtifactInteractionProto : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        SceneManager.LoadScene(gameOverScene);
+        
         fadeOutObj.SetActive(true);
+        //Debug.Log("4 linha antes da Coroutine"); - Não entra aqui
+
+        /*
         var input = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<InputHandler>();
+        Debug.Log("3 linha antes da Coroutine");
         input.GetJumpCommand().SetCanJump(false);
+        Debug.Log("2 linha antes da Coroutine");
         input.canWalk = false;
+        Debug.Log("1 linha antes da Coroutine");
         StartCoroutine(GoToGameOver());
+        */
     }
 
     IEnumerator GoToGameOver()
     {
         yield return new WaitForSeconds(timeToGameOver);
-
         SceneManager.LoadScene(gameOverScene);
+
     }
 }
