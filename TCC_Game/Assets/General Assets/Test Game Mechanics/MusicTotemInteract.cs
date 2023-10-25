151082e84c37de11a6eeed63d0445058aaf2f575
yayamiho <yayamiho@gmail.com>
2023-10-24 21:43:00 -0300

    4

--- TCC_Game/Assets/General Assets/Test Game Mechanics/MusicTotemInteract.cs
+++ TCC_Game/Assets/General Assets/Test Game Mechanics/MusicTotemInteract.cs
@@ -70,6 +70,10 @@ public class MusicTotemInteract : MonoBehaviour, IInteractable
 
         else if (activeAnim != null)
             activeAnim.ActivatedTotem();
+
+
+        if (musicPuzzle == null)
+            StartShine();
     }
     #endregion
 
