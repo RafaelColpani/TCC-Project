1ca3cdf84dd50ef290db6eb01b35aaaf257a3149
Henrique Assis <henriquebatista061@gmail.com>
2023-11-15 01:09:42 -0300

    Update SequentialPlatformPuzzle.cs

--- TCC_Game/Assets/General Assets/Puzzles/Scripts/SequentialPlatformPuzzle.cs
+++ TCC_Game/Assets/General Assets/Puzzles/Scripts/SequentialPlatformPuzzle.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KevinCastejon.MoreAttributes;
 
 public class SequentialPlatformPuzzle : MonoBehaviour
 {
-    [SerializeField] Transform[] teste;
+    [HeaderPlus(" ", "- GRID -", (int)HeaderPlusColor.green)]
+    [Tooltip("The number of rows that the grid have")]
+    [SerializeField] int rowsCount;
+    [SerializeField] int columnsCount;
 }
