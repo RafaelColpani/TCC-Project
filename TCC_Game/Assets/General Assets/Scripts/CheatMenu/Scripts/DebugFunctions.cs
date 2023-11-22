5380c550381fedbfb6ee842e6545ddff44664995
RafaelColpani <rafael.colpani14@outlook.com>
2023-11-21 23:06:02 -0300

    Add: +Debug | Sound Video

--- TCC_Game/Assets/General Assets/Scripts/CheatMenu/Scripts/DebugFunctions.cs
+++ TCC_Game/Assets/General Assets/Scripts/CheatMenu/Scripts/DebugFunctions.cs
@@ -11,6 +11,10 @@ public class DebugFunctions : MonoBehaviour
         DebugController.AddCommand("next_scene", "Avança uma cena", "next_scene", D_NextScene);
         DebugController.AddCommand("restart", "Recomeca a cena", "restart", D_Restart);
         DebugController.AddCommand("undo_scene", "Volta uma cena", "undo_scene", D_Undo);
+
+        DebugController.AddCommand("scene_summer", "Va para o verao", "undo_scene", D_Summer);
+        DebugController.AddCommand("scene_fall", "Va para o outono", "undo_scene", D_Fall);
+        DebugController.AddCommand("scene_winter", "va para o inverno", "undo_scene", D_Winter);
     }
     #endregion
 
@@ -30,6 +34,24 @@ public class DebugFunctions : MonoBehaviour
     public void D_Undo()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        
+    }
+
+    public void D_Summer()
+    {
+        SceneManager.LoadScene("Puzzle1");
+        
+    }
+
+    public void D_Fall()
+    {
+        SceneManager.LoadScene("Autumn-Puzzle1");
+        
+    }
+
+    public void D_Winter()
+    {
+        SceneManager.LoadScene("Winter-Tutorial");
         
     }
     #endregion
