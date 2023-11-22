23bf5677f80adb389549f60d4d3acf2ba2774add
Henrique Assis <henriquebatista061@gmail.com>
2023-11-21 23:54:44 -0300

    fix: grids

--- TCC_Game/Assets/General Assets/Puzzles/Scripts/SequentialPlatformPuzzle.cs
+++ TCC_Game/Assets/General Assets/Puzzles/Scripts/SequentialPlatformPuzzle.cs
@@ -57,6 +57,8 @@ public class PlatformGrid
 public class SequentialPlatformPuzzle : MonoBehaviour
 {
     #region Inspector Vars
+    [SerializeField] ProceduralLegs playerLegs;
+
     [HeaderPlus(" ", "- GRID CONSTRUCTOR -", (int)HeaderPlusColor.green)]
     [Tooltip("The prefab of each platform that will compose the grid")]
     [SerializeField] GameObject platformPrefab;
@@ -121,8 +123,6 @@ public class SequentialPlatformPuzzle : MonoBehaviour
     private List<GameObject> enemyTraps;
     private PlatformGrid previousPlatformVisited = null;
 
-    private ProceduralLegs playerLegs;
-
     private bool gridConstructed = false;
     private bool initiatedPuzzle = false;
 
@@ -142,10 +142,10 @@ public class SequentialPlatformPuzzle : MonoBehaviour
             shaderVFX.transform.localScale = Vector3.zero;
     }
 
-    private void Start()
-    {
-        playerLegs = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ProceduralLegs>();
-    }
+    //private void Start()
+    //{
+    //    playerLegs = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ProceduralLegs>();
+    //}
 
     private void Update()
     {
@@ -180,14 +180,8 @@ public class SequentialPlatformPuzzle : MonoBehaviour
     #region Private Methods
     private PlatformGrid GetStepedPlatform()
     {
-        playerLegs = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ProceduralLegs>();
-
-        Debug.Log("Bug acusado aqui.");
-        Debug.Log($"playerLegs: {playerLegs} | groundedObject: {playerLegs.GetGroundedObject()}");
         if (playerLegs.GetGroundedObject() == null)
             return null;
-        Debug.Log("Pós acusação de bug.");
-        Debug.Log($"playerLegs: {playerLegs} | groundedObject: {playerLegs.GetGroundedObject()}");
 
         var groundedObject = playerLegs.GetGroundedObject();
 
